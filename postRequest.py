2752ac1efde8687852b4d51c88711d4fefbd7ea5
PenguDExiled <114444189+PenguDExiled@users.noreply.github.com>
2022-09-27 14:32:59 +0200

    Add files via upload

---
+++ postRequest.py
@@ -0,0 +1,10 @@
+import requests
+
+"""
+Makes a post request to my database.
+"""
+
+def addData(augment, rarity, stage, user, gameid):
+    parameters = {"augment": augment, "rarity": rarity, "stage": stage, "username": user, "gameid": gameid}
+    req = requests.post("https://apex.oracle.com/pls/apex/leonworkspace/new/augment", data=parameters)
+    print(req.status_code)
